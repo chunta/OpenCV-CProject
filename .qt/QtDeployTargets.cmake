feeee5fd912df584242a8f34cd554da53888becc
Rex Chen <rexchen@RexdeMac-mini.local>
2024-07-16 18:00:59 +0800

    launch a qt window

---
+++ .qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_MyApp_FILE /Users/rexchen/workspace/OpenCV-CProject/MyApp)
+set(__QT_DEPLOY_TARGET_MyApp_TYPE EXECUTABLE)
--- main.cpp
+++ main.cpp
@@ -1,31 +1,12 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
+#include <QApplication>
+#include <QLabel>
 
-int main() {
-    cv::Mat image = cv::imread("car-967387_640.png");
-    if (image.empty()) {
-        std::cerr << "Could not open or find the image" << std::endl;
-        return -1;
-    }
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
 
-     // Apply Gaussian blur
-    cv::Mat blurredImage;
-    cv::GaussianBlur(image, blurredImage, cv::Size(15, 15), 0);
+    QLabel label("Hello, Qt!");
+    label.show();
 
-    // Convert to grayscale
-    cv::Mat grayImage;
-    cv::cvtColor(image, grayImage, cv::COLOR_BGR2GRAY);
-
-     // Display the original and blurred images
-    cv::namedWindow("Original Image", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Original Image", image);
-
-    cv::namedWindow("Blurred Image", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Blurred Image", blurredImage);
-
-    cv::namedWindow("Grayscale Image", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Grayscale Image", grayImage);
-
-    cv::waitKey(0);
-    return 0;
+    return app.exec();
 }
